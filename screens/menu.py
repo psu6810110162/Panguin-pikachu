b407d523dcb9639a901accc09f6486b14a458115
Teerayut <6810110162@psu.ac.th>
2026-03-06 01:14:53 +0700

    fix:update Sounds

--- screens/menu.py
+++ screens/menu.py
@@ -8,6 +8,22 @@ class MenuScreen(Screen):
         AudioManager().play_bgm('Bgm.gameplay.mp3')
 
     def start_game(self):
+        AudioManager().play_sfx('click')
         # สั่งให้ ScreenManager เปลี่ยนหน้าไปที่ 'gameplay'
         logger.info("กำลังเริ่มต้นเกม...")
         self.manager.current = 'gameplay'
+    
+    def go_to_history(self):
+        AudioManager().play_sfx('click')
+        logger.info("ไปที่หน้าประวัติการเล่น...")
+        self.manager.current = 'history'
+
+    def go_to_shop(self):
+        AudioManager().play_sfx('click')
+        logger.info("ไปที่หน้าร้านค้า...")
+        self.manager.current = 'shop'
+
+    def exit_game(self):
+        AudioManager().play_sfx('click')
+        from kivy.app import App
+        App.get_running_app().stop()
