63a7d0086f2088881e815e48fbafe5ad2ff3472b
Teerayut <6810110162@psu.ac.th>
2026-03-05 22:56:46 +0700

    fix:update SoundsBrackground in menu

--- screens/menu.py
+++ screens/menu.py
@@ -1,9 +1,11 @@
 from kivy.uix.screenmanager import Screen
+from core.audio import AudioManager
 from core.logger import logger
 
 class MenuScreen(Screen):
     def on_enter(self):
         logger.info("เข้าสู่หน้าจอ MenuScreen")
+        AudioManager().play_bgm('Bgm.gameplay.mp3')
 
     def start_game(self):
         # สั่งให้ ScreenManager เปลี่ยนหน้าไปที่ 'gameplay'
