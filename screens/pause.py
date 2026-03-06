4f1d0625f10c130bda467ea9cca324c5195d1831
Teerayut <6810110162@psu.ac.th>
2026-03-06 14:49:37 +0700

    fix: add missing SFX keys and normalize sfx name to lowercase

--- screens/pause.py
+++ screens/pause.py
@@ -1,18 +1,31 @@
 from kivy.uix.screenmanager import Screen
 from core.logger import logger
+from core.audio import AudioManager
+from kivy.clock import Clock
 
 class PauseScreen(Screen):
     def on_enter(self):
         logger.info("เข้าสู่หน้าจอ Pause")
+
         
     def resume_game(self):
-        logger.info("เล่นต่อ...")
-        self.manager.current = 'gameplay'
+        AudioManager().play_sfx('click')
+        Clock.schedule_once(lambda dt: self._go_gameplay(), 0.2)
+
         
     def restart_game(self):
-        logger.info("เริ่มเกมใหม่จากหน้าจอ Pause...")
-        self.manager.current = 'gameplay'
+        AudioManager().play_sfx('click')
+        Clock.schedule_once(lambda dt: self._go_gameplay(), 0.2)
+
         
     def go_home(self):
-        logger.info("กลับไปหน้าเมนูหลัก...")
+        AudioManager().play_sfx('click')
+        Clock.schedule_once(lambda dt: self._go_menu(), 0.2)
+        
+
+    def _go_gameplay(self):
+        self.manager.current = 'gameplay'
+
+
+    def _go_menu(self):
         self.manager.current = 'menu'
